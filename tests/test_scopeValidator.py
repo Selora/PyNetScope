c18378489dfb7b289cf3dbf27f6be796aca9e9ad
selora <selora@selora.com>
2018-02-16 18:12:47 -0500

    Initial Commit

---
+++ tests/test_scopeValidator.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestScopeValidator(TestCase):
+    def test_validate_ip(self):
+        self.fail()
+
+    def test_validate_hostname(self):
+        self.fail()
+
+    def test_validate_host_csv(self):
+        self.fail()
+
+    def test_validated_scope_writer(self):
+        self.fail()
