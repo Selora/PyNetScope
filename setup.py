5115eae3c60e0016e378ae442764374cc0b87e51
Selora <sebastien.lorrain01@gmail.com>
2020-03-27 15:59:30 +0000

    FIX: Updated setup.py for new directory tree

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='PyNetScope',
     version='0.1.1',
-    packages=['tests'],
+    packages=['PyNetScope', 'PyNetScope.tests'],
     url='https://github.com/Selora/PyNetScope',
     license='BSD-2',
     author='Selora',
     author_email='',
-    description='Python library to healp with external pentest Recon (FQDN,IP,NetRange'
+    description='Python library to healp with external pentest Recon (FQDN,IP,NetRange)'
 )
